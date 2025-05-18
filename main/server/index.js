9521dfe1794e3b5e87c8520e93b9e70b134ad1b6
tanwartushar <>
2025-05-18 09:24:13 +0800

    Setup login and signup pages

---
+++ main/server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { Pool } = require('pg');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+// middleware
+// const corsOptions = {
+//     origin: 'http://localhost:3001',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true
+// };
+
+app.use(cors());
+app.use(express.json());    // parse JSON requests
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+
+// test db connection 
+pool.connect()
+    .then(() => console.log('Connected to PostgreSQL'))
+    .catch(err => console.error('Error connecting to PostgreSQL:', err));
+
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    console.log('GET / route hit');
+    res.send('Online Marketplace API is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
